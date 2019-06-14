fe71edf04bf567e2b3dcabefafa15d5c8c10a9f2
koboos <koboos@gmail.com>
2019-06-14 22:30:26 +0200

    Initial commit

---
+++ src/main/java/com/koboos/exchange_diffs/ExchangeDiffsApplication.java
@@ -0,0 +1,13 @@
+package com.koboos.exchange_diffs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeDiffsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeDiffsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koboos/exchange_diffs/ExchangeDiffsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.koboos.exchange_diffs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExchangeDiffsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
